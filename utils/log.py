6245469ba8839e9dbe6a241b4fca9fe1db1de164
Marcel F <33006171+mfroemmi@users.noreply.github.com>
2022-10-11 16:22:07 +0200

    added timestamp in log messages

--- utils/log.py
+++ utils/log.py
@@ -1,31 +1,49 @@
 import inspect
 import pathlib
+from datetime import datetime
 from types import FrameType
 from termcolor import colored
 
 
+"""
+Message Log Class
+
+:method l - log
+:method d - debug
+:method w - warning
+"""
+
+
 def l(frame: FrameType, message):
-    path = pathlib.PurePath(inspect.getframeinfo(frame).filename)
-    frameInfo = path.name, inspect.getframeinfo(frame).lineno
-    frameInfoFormatted = formatString(str(frameInfo), 50)
-    print(colored(formatString("log", 10), "green"), colored(frameInfoFormatted, "green"), message)
+    frameInfoFormatted = formatFrame(frame)
+    print(getTimeStamp(), "\t", colored(formatString("log", 10), "green"), colored(frameInfoFormatted, "green"), message)
 
 
 def d(frame: FrameType, message):
-    path = pathlib.PurePath(inspect.getframeinfo(frame).filename)
-    frameInfo = path.name, inspect.getframeinfo(frame).lineno
-    frameInfoFormatted = formatString(str(frameInfo), 50)
-    print(colored(formatString("debug", 10), "blue"), colored(frameInfoFormatted, "blue"), message)
+    frameInfoFormatted = formatFrame(frame)
+    print(getTimeStamp(), "\t", colored(formatString("debug", 10), "blue"), colored(frameInfoFormatted, "blue"), message)
 
 
 def w(frame: FrameType, message):
+    frameInfoFormatted = formatFrame(frame)
+    print(getTimeStamp(), "\t", colored(formatString("warning", 10), "yellow"), colored(frameInfoFormatted, "yellow"), message)
+
+
+def formatFrame(frame: FrameType):
     path = pathlib.PurePath(inspect.getframeinfo(frame).filename)
     frameInfo = path.name, inspect.getframeinfo(frame).lineno
     frameInfoFormatted = formatString(str(frameInfo), 50)
-    print(colored(formatString("warning", 10), "yellow"), colored(frameInfoFormatted, "yellow"), message)
+    return frameInfoFormatted
 
 
 def formatString(name: str, lenght: int):
     for i in range(lenght - len(name)):
         name += " "
     return name
+
+
+def getTimeStamp():
+    current_time = datetime.now()
+    time_stamp = current_time.timestamp()
+    date_time = datetime.fromtimestamp(time_stamp)
+    return date_time
